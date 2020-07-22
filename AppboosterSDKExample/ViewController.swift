c8ad8ab15921c42ab66c14399b80f487095ce232
VladimirKhuraskin <khuraskin.dev@gmail.com>
2020-07-22 14:53:47 +0400

    Configure base lib

---
+++ AppboosterSDKExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AppboosterSDKExample
+//
+//  Created by Appbooster on 22.07.2020.
+//  Copyright © 2020 Appbooster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+}
