8322cddfe1be9ce70e9f8bf05f16994557336dff
VladimirKhuraskin <khuraskin.dev@gmail.com>
2020-07-23 10:45:43 +0400

    Setup Example

--- AppboosterSDKExample/ViewController.swift
+++ AppboosterSDKExample/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    let ab = AppboosterAB(authToken: "<APITAPI_AUTH_TOKEN>",
+                          deviceToken: "<DEVICE_TOKEN")
+    ab.fetch(knownKeys: ["<TEST_1_KEY>", "<TEST_2_KEY>"],
+             completion: { abError in
+              guard abError == nil else { return }
+
+              let test1Value: String? = ab.value("<TEST_1_KEY>")
+              let test2Value: Int = ab.value("<TEST_2_KEY>", or: 3)
+
+              print(test1Value ?? "", test2Value)
+    })
   }
 
 }
