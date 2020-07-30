a588ea4f761ef930fdbbc31302b8abb53335f869
VladimirKhuraskin <khuraskin.dev@gmail.com>
2020-07-30 12:49:43 +0400

    Added SPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppboosterSDK",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "AppboosterSDK",
+            targets: ["AppboosterSDK"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "AppboosterSDK",
+            path: "AppboosterSDK"
+        ),
+    ]
+)
